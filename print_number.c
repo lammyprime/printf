9ea03d8a2062352ccab24dd914b2b8632fc7622c
Bunmi <monehinolubunmi1@gmailcom>
2022-11-21 11:02:22 -0800

    main.c and h file

--- print_number.c
+++ print_number.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_long_number - prints a long integer
+ * print_number - prints an integer number using an array
  * @args: arguments
  *
  * Return: count
  */
-int print_long_number(va_list args)
+int print_number(va_list args)
 {
-	long int num;
-	unsigned long int num_t, temp, i, div = 1, count = 0;
+	int num;
+	unsigned int num_t, temp, i, div = 1, count = 0;
 
-	num = va_arg(args, long int);
+	num = va_arg(args, int);
 	if (num < 0)
 	{
 		num_t = num * -1;
@@ -39,15 +39,15 @@ int print_long_number(va_list args)
 }
 
 /**
- * print_short_number - prints a short integer number using an array
+ * print_plus_number - prints an integer number using an array
  * @args: arguments
  *
  * Return: count
  */
-int print_short_number(va_list args)
+int print_plus_number(va_list args)
 {
-	short int num;
-	unsigned short int num_t, temp, i, div = 1, count = 0;
+	int num;
+	unsigned int num_t, temp, i, div = 1, count = 0;
 
 	num = va_arg(args, int);
 	if (num < 0)
@@ -67,6 +67,54 @@ int print_short_number(va_list args)
 	{
 		div *= 10;
 		temp /= 10;
+	}
+
+	if (count == 0)
+	{
+		_putchar('+');
+		count++;
+	}
+	for (i = 0; div > 0; div /= 10, i++, count++)
+	{
+		_putchar(((num_t / div) % 10) + '0');
+	}
+	return (count);
+}
+
+/**
+ * print_space_number - prints an integer number using an array
+ * @args: arguments
+ *
+ * Return: count
+ */
+int print_space_number(va_list args)
+{
+	int num;
+	unsigned int num_t, temp, i, div = 1, count = 0;
+
+	num = va_arg(args, int);
+	if (num < 0)
+	{
+		num_t = num * -1;
+		_putchar('-');
+		count++;
+	}
+	else
+	{
+		num_t = num;
+	}
+
+	temp = num_t;
+
+	while (temp > 9)
+	{
+		div *= 10;
+		temp /= 10;
+	}
+	if (count == 0)
+	{
+		_putchar(' ');
+		count++;
 	}
 
 	for (i = 0; div > 0; div /= 10, i++, count++)
